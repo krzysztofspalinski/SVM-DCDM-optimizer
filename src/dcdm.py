5eeaac5a723fb42547cf85d2279c05aab32cabcb
= <=>
2021-01-27 02:04:44 +0100

    Optimality condition

--- src/dcdm.py
+++ src/dcdm.py
@@ -9,15 +9,16 @@ class DCDM(Optimizer):
     def __init__(self):
         pass
 
-    def optimize(self, model, X, y, eps=10**-1):
+    def optimize(self, model, X, y, eps=5*10**-1):
         if model.w is None:
             model._Model__initialize_parameters(X.shape[1])
         U = model.C
         Q_conj_diag = np.einsum('ij,ji->i', X, X.T)
         alpha = np.zeros_like(y, dtype=float)
         iter_no = 0
-        while iter_no < 10**3:  # WARUNEK OPTYMALNOŚCI
-            iter_no += 1
+        loss_history_gain = np.full(5, np.inf)
+        while True:
+            old_loss = model.compute_loss(X, y)
             for i in np.random.permutation(range(len(alpha))):
                 alpha_i = alpha[i]
                 alpha_i_conj = alpha_i
@@ -33,3 +34,9 @@ class DCDM(Optimizer):
                         max(alpha_i - G/Q_conj_diag[i], 0), U)
                     model.w += ((alpha[i] - alpha_i_conj) *
                                 y[i] * X[i].T).reshape(-1, 1)
+            new_loss = model.compute_loss(X, y)
+            loss_history_gain[iter_no % 5] = np.abs(old_loss - new_loss)
+            if np.std(loss_history_gain)/np.max(loss_history_gain+eps) < eps:
+                return
+            else:
+                iter_no += 1
